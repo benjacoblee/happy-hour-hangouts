8c68440349180a91df7697587a1cb2a0b4560851
benjacoblee <benjacoblee@gmail.com>
2020-02-03 19:02:00 +0800

    test conditional routing

--- public/bookmarkBar.js
+++ public/bookmarkBar.js
@@ -11,7 +11,9 @@ const bookmarkHandler = function() {
 bookmarkButton.addEventListener("click", () => {
   const bookmarkRequest = new XMLHttpRequest();
   bookmarkRequest.addEventListener("load", bookmarkHandler);
-  const favoriteURL = "http://localhost:3000/bars/favorite";
+  const favoriteURL =
+    "http://localhost:3000/bars/favorite" ||
+    "https://murmuring-thicket-55526.herokuapp.com/bars/favorite";
   bookmarkRequest.open("GET", favoriteURL);
   bookmarkRequest.send();
 });
--- public/checkFavorite.js
+++ public/checkFavorite.js
@@ -15,7 +15,9 @@ const request = new XMLHttpRequest();
 request.addEventListener("load", checkIfBookmarked);
 
 // ready the system by calling open, and specifying the url
-var url = "http://localhost:3000/bars/checkfavorite";
+var url =
+  "http://localhost:3000/bars/checkfavorite" ||
+  "https://murmuring-thicket-55526.herokuapp.com/bars/checkfavorite";
 request.open("GET", url);
 
 // send the request
